7760a17e1c9588ab29371ddabf448374215fa4e3
Jonathan Guyer <guyer@nist.gov>
2022-03-05 16:10:05 -0500

    Raise ValueError for negative inflammations

--- inflammation/models.py
+++ inflammation/models.py
@@ -54,6 +54,8 @@ def patient_normalize(data):
 
     Negative values are rounded to 0.
     """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
     max_data = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalized = data / max_data[:, np.newaxis]
